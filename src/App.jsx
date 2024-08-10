359abc3bcdfc4ddd77bf8cd5f05f8939e60a97fa
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-08-10 19:35:37 +0200

    Get selected places from localStorage on page loading

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,17 @@ import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import { sortPlacesByDistance } from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map((id) => {
+  return AVAILABLE_PLACES.find((place) => place.id === id);
+});
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -59,6 +65,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
